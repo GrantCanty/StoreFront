8a082ffeba4fd4ee477139efb4b32770a8896d08
GrantCanty <gc933815@ohio.edu>
2023-11-12 04:43:21 -0700

    changes to navbar to show menu on hover. changes to how state is stored for each dropdown

--- client/storefront/src/assets/dropdown.tsx
+++ client/storefront/src/assets/dropdown.tsx
@@ -1,16 +1,22 @@
 import { ReactElement } from "react";
+import './dropdown.css'
 
-const DropDown: React.FC = (): ReactElement => {
+interface DropDownProps {
+    show: boolean
+}
+
+const DropDown: React.FC<DropDownProps> = (props): ReactElement => {
+    let disp: string
+    props.show ? disp = "block" : disp = "none"
+    
     return (
-        <>
-            <div className="dropdown">
+            <div className="dropdown" style={{"display": disp}}>
                 <ul>
                     <li>item 1</li>
                     <li>item 2</li>
                     <li>item 3</li>
                 </ul>
             </div>
-        </>
     )
 }
 
--- client/storefront/src/assets/navbar.tsx
+++ client/storefront/src/assets/navbar.tsx
@@ -1,35 +1,44 @@
 import { ReactElement, useState, useRef } from "react";
 import './navbar.css'
 import DropDown from "./dropdown";
-import useOnHover from "../hooks/onHover";
 import { Link } from "react-router-dom";
 
 const NavBar: React.FC = (): ReactElement => {
-    const dropDownRef = useRef<HTMLInputElement>(null)
-    const [isDropDownVisible, setDropDownVisible] = useState(false)
 
-    const closeHoverMenu = () => {
-        setDropDownVisible(false)
+    const arr: boolean[] = Array<boolean>(4)
+    const [isDropDownVisible, setDropDownVisible] = useState<Array<boolean>>(arr)
+
+    const mouseOn = (id: number) => {
+        let arr: boolean[] = Array<boolean>(4)
+        arr[id] = true
+        setDropDownVisible(arr)
     }
 
-    useOnHover(dropDownRef, closeHoverMenu)
+    const mouseOff = (id: number) => {
+        let arr: boolean[] = Array<boolean>(4)
+        arr[id] = false
+        setDropDownVisible(arr)
+    }
     
     return (
         <nav id="menu" className="navbar">
             <nav className="inner-navbar">
                 <ul className="navbar-items">
-                    <li className="navbar-item" onMouseOver={ () => setDropDownVisible(true)}>
+                    <li className="navbar-item" onMouseOver={ () => mouseOn(0) } onMouseLeave={ () => mouseOff(0) }>
                         <Link to='/'>New Arrivals</Link>
+                        <DropDown show={isDropDownVisible[0]} />
                     </li>
-                    <li className="navbar-item" onMouseOver={ () => setDropDownVisible(true)} >
+                    <li className="navbar-item" onMouseOver={ () => mouseOn(1) } onMouseLeave={ () => mouseOff(1) }>
                         <Link to='/'>Women</Link>
-                        {/*<DropDown />*/}
+                        <DropDown show={isDropDownVisible[1]} />
                     </li>
-                    <li className="navbar-item" onMouseOver={ () => setDropDownVisible(true)} >
+                    <li className="navbar-item" onMouseOver={ () => mouseOn(2) } onMouseLeave={ () => mouseOff(2) }>
                         <Link to='/'>Men</Link>
+                        <DropDown show={isDropDownVisible[2]} />
                     </li>
-                    <li className="navbar-item" onMouseOver={ () => setDropDownVisible(true)} >
+                    <li className="navbar-item" onMouseOver={ () => mouseOn(3) } onMouseLeave={ () => mouseOff(3) }>
                         <Link to='/'>Accessories</Link>
+                        <DropDown show={isDropDownVisible[3]} />
                     </li>
                 </ul>
             </nav>
