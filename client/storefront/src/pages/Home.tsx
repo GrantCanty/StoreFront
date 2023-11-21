0c52ebe1e6d2f9a6a4c626a1d1c9ecb86000a2ff
GrantCanty <gc933815@ohio.edu>
2023-11-21 00:09:12 -0700

    add interfaces and map to home.tsx page. mapped over map instead of hardcoding <li> elements

--- client/storefront/src/pages/Home.tsx
+++ client/storefront/src/pages/Home.tsx
@@ -1,32 +1,36 @@
 import { ReactElement } from "react";
 import { Link } from "react-router-dom";
 
+interface ShopDetails {
+    category: string;
+    gender: string;
+
+}
+
+interface MapDetails {
+    url: string;
+    filters: ShopDetails;
+}
+
 const Home: React.FC = (): ReactElement => {
+    let mapData = new Map<string, MapDetails>();
+    mapData.set("New Arrivals", {url: "na", filters: {category: "New Arrivals", gender: ""}})
+    mapData.set("Women", {url: "wmn", filters: {category: "", gender: "Women"}})
+    mapData.set("Men", {url: "mn", filters: {category: "", gender: "Men"}})
+    mapData.set("Denim", {url: "dnm", filters: {category: "Denim", gender: ""}})
+    mapData.set("Tops", {url: "tp", filters: {category: "Tops", gender: ""}})
+    mapData.set("Jeans", {url: "jn", filters: {category: "Jeans", gender: ""}})
+    mapData.set("Accessories", {url: "acsrs", filters: {category: "Accessories", gender: ""}})
+    
     return (
     <>
         <h1>Home</h1>
         <ul>
-            <li>
-                <Link to="/shop" state={{category: "new arrivals"}} >New Arrivals</Link>
-            </li>
-            <li>
-                <Link to="/shop" state={{gender: "women"}} >Women</Link>
-            </li>
-            <li>
-                <Link to="/shop" state={{gender: "men"}} >Men</Link>
-            </li>
-            <li>
-                <Link to="/shop" state={{category: "denim"}} >Denim</Link>
-            </li>
-            <li>
-                <Link to="/shop" state={{category: "tops"}} >Tops</Link>
-            </li>
-            <li>
-                <Link to="/shop" state={{category: "jeans"}} >Jeans</Link>
-            </li>
-            <li>
-                <Link to="/shop" state={{category: "accessories"}} >Accesories</Link>
-            </li>
+            {
+                Array.from(mapData.keys()).map((key: string) => {
+                    return <li onMouseEnter={ () => console.log(mapData.get(key)) }><Link to="/shop" state={mapData.get(key)?.filters} >{key}</Link></li>
+                })
+            }
         </ul>
     </>
     )
