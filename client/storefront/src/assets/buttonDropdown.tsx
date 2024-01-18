01056be481b08c0543d286939ed758c5d8b04c2e
GrantCanty <gc933815@ohio.edu>
2024-01-18 00:53:09 -0700

    added redirect to buttonDropdown. redirect is currently hardcoded

--- client/storefront/src/assets/buttonDropdown.tsx
+++ client/storefront/src/assets/buttonDropdown.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
     dropDownItems: string[]
@@ -9,11 +10,15 @@ const ButtonDropDown: React.FC<Props> = (props): ReactElement => {
     let disp: string
     props.show ? disp = "block" : disp = "none"
 
+    const nav = useNavigate()
+
     return (
         <div style={{"display": disp, "position": "absolute"}}>
             <ul>
                 {props.dropDownItems.map((str: string, i: number) => {
-                    return <li key={i}>{str}</li>
+                    return <li 
+                        key={i}
+                        onClick={ () => nav("/shop", { state: {category: "Jeans", gender: "Men"} }) }> {str} </li>
                 })}
             </ul>
         </div>
